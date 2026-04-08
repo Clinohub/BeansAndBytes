962e0c88726e6c67ae770dc464c91dc1658fa8ae
Clinohub <esmicorreo09@gmail.com>
2026-04-08 14:54:33 +0300

    My first Java project with eclipse

---
+++ Basics/MyFirstJavaProject/firstjavaproject/MutliplicationTableRunner.java
@@ -0,0 +1,13 @@
+package firstjavaproject;
+
+public class MutliplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		// table.print();
+		table.print(8);
+		// table.print(12, 1, 10);
+
+	}
+
+}
