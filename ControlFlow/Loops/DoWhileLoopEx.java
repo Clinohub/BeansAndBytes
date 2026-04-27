3d7d62cd6d30077a5e385ca1e1db6b20e4cde40e
Clinohub <esmicorreo09@gmail.com>
2026-04-27 10:47:57 +0300

    do while loops

---
+++ ControlFlow/Loops/DoWhileLoopEx.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DoWhileLoopEx {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+
+        do {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+            if (number >= 0)
+                System.out.println("Cube is " + number*number*number);
+        } while (number >= 0);
+
+        System.out.println("Thank You! Have Fun");
+
+        scanner.close();
+    }
+}
