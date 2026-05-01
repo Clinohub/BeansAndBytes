3185ec3b0403ed6f3f1658cf0685b80b2000ba88
Clinohub <esmicorreo09@gmail.com>
2026-05-01 08:24:29 +0300

    update method

--- Basics/Arrays/DaysOfWeekArray.java
+++ Basics/Arrays/DaysOfWeekArray.java
@@ -1,6 +1,29 @@
 import java.util.Arrays;
 
 public class DaysOfWeekArray {
+ 
+    String daysOfWeek (String... daysOfWeek) {
+        String day = daysOfWeek[0];
+        for (String dayOfWeek: daysOfWeek) {
+            if (dayOfWeek.length() > day.length()) {
+                day = dayOfWeek;
+            }
+        }
+        
+        return day;
+    }
+
+
+    String[] daysOfWeekReverse (String... daysOfWeek) {
+        int j = daysOfWeek.length;
+        String dayReverse[] = new String[j];
+
+        for (int i = 0; i < daysOfWeek.length; i++) {
+            dayReverse[i] = daysOfWeek[--j]; 
+        }
+
+        return dayReverse;
+    }
 
     public static void main(String[] args) {
         DaysOfWeekArray daysOfWeekArray = new DaysOfWeekArray();
